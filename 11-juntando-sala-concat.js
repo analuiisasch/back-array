8804cb596184905ce9115702a77b27abdef710ab
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-06 11:45:54 -0300

    Add files via upload

---
+++ 11-juntando-sala-concat.js
@@ -0,0 +1,5 @@
+const salaJS = ['Ana', 'Beatriz', 'Emilyn', 'Giovana'];
+const salaPython = ['Maria Eduarda', 'Sthefanie', 'Mileni', 'Nicoli'];
+
+const salaUnificadas = salaJS.concat(salaPython);
+console.log(salaUnificadas)
