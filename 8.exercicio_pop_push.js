dd5a705b4643dc05ea221dded62f51dc3dff971b
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-06 10:59:14 -0300

    Add files via upload

---
+++ 8.exercicio_pop_push.js
@@ -0,0 +1,13 @@
+jantarDehoje = ['🍔','🌭', '🍕' ]
+
+jantarDehoje.pop()
+jantarDehoje.pop()
+
+
+jantarDehoje.push('🍳')
+jantarDehoje.push('🥗')
+jantarDehoje.push('🍏')
+
+//qual é o resultado?
+
+console.log(jantarDehoje)
