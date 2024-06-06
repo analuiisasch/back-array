a39a89dfc98b6a0ab1a668db39c0ba342f7b1886
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-06 11:16:33 -0300

    Add files via upload

---
+++ 9-remover-meio-splice.js
@@ -0,0 +1,16 @@
+const alunos = ['joão', 'ana', 'caio', 'Lara', 'Marjorie', 'Leo']
+
+//splice = significa emendar, juntar 
+// o primeiro indica o índice do primeiro elemento a retirar e o segundo indica
+//o índice do último
+// O metódo splice altera o array original (diferente do splice)
+
+
+alunos.splice(1,1)
+console.log(alunos)
+
+//alunos.splice(1,2,"Rodrigo")//Permite um terceiro parâmetro 
+//console.log(aluno)
+
+
+*neste codigo o joao é o 0, neste caso tirou a ana que é o 1 .    SEMPRE COMEÇA NO 0 
