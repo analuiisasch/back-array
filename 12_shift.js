eaba3a9a17938c060644392c45afa3ec3e8f2a80
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-13 10:50:01 -0300

    Add files via upload

---
+++ 12_shift.js
@@ -0,0 +1,6 @@
+//Retirar o primeiro elemento utilizandoo método*** shift***
+let retirar_primeiro = [2,3,4,5,6,7,8,9,10,11,12]
+retirar_primeiro.shift()
+console.log(retirar_primeiro)
+retirar_primeiro[0] = 100 // inclusão do valor 100 no índice 0(indexação)
+console.log(retirar_primeiro)
