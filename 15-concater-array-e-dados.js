bc95f49d9269a33f69d4127d8952e19426251450
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-13 11:53:46 -0300

    Update 15-concater-array-e-dados.js

--- 15-concater-array-e-dados.js
+++ 15-concater-array-e-dados.js
@@ -11,3 +11,15 @@ const arrayConcat2 = arrayOriginal_2.concat(["André", "Fernanda"], ["Ricardo","
  console.log(arrayConcat2)
  //Podemos passar mais de um array como parãmetro para que seus elementos sejam concatenados em
  //um único array, junto com o que está sendo chamado com o método (no caso, arrayoriginal)
+
+
+
+resposta 
+
+4) ['Maria', 'Carlos', 'Eduardo', 'Samanta']
+15-concater-array-e-dados.js:3
+(6) ['Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda']
+15-concater-array-e-dados.js:4
+(4) ['Maria', 'Carlos', 'Eduardo', 'Samanta']
+15-concater-array-e-dados.js:10
+(10) ['Maria', 'Carlos', 'Eduardo', 'Samanta', 'André', 'Fernanda', 'Ricardo', 'Ana', 'Marcelo', 'Bia']
