5cdb59579ff912f8d7b6d736a070d38eb1df3f0b
ana luisa <140508741+analuiisasch@users.noreply.github.com>
2024-06-13 10:50:41 -0300

    Update 12_shift.js

--- 12_shift.js
+++ 12_shift.js
@@ -4,3 +4,5 @@ retirar_primeiro.shift()
 console.log(retirar_primeiro)
 retirar_primeiro[0] = 100 // inclusão do valor 100 no índice 0(indexação)
 console.log(retirar_primeiro)
+
+shift adiciona no primeiro lugar
